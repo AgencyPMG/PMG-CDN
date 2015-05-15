d713836a5db47db0a877d60cec079424c29ffdf9
Christopher Davis <cdavis9999@gmail.com>
2015-05-15 16:07:36 -0400

    GPL -> MIT
    Also clean up the plugin head.

--- pmg-cdn.php
+++ pmg-cdn.php
@@ -1,29 +1,40 @@
 <?php
-/*
-Plugin Name: PMG CDN
-Plugin URI: http://pmg.co
-Description: A simple plugin that enables an origin pull CDN on your WordPress site.
-Version: 1.0
-Text Domain: pmgcdn
-Author: Christopher Davis
-Author URI: http://pmg.co/people/chris
-License: GPL2
+/**
+ * Plugin Name: PMG CDN
+ * Plugin URI: https://github.com/AgencyPMG/pmg-cdn
+ * Description: A simple origin-pull CDN for yoru WordPress site.
+ * Version: 1.0
+ * Text Domain: pmgcdn
+ * Author: Christopher Davis
+ * Author URI: http://christopherdavis.me
+ * License: MIT
+ *
+ * Copyright (c) 2015 PMG <https://www.pmg.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * @category    WordPress
+ * @author      Christopher Davis <http://christopherdavis.me>
+ * @copyright   2015 MIT
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
 
-    Copyright 2012 Performance Media Group <seo@pmg.co>
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
 
 !defined('ABSPATH') && exit;
 
